16a19f5f6b0d40a01e93efe9b5d18d9599cfe4f1
Eric Hernandez <eherna97@ucsc.edu>
2021-03-07 15:42:49 -0800

    added some functions that will theoretically move the snake (I have only once touched pygames so we'll see

--- snake.py
+++ snake.py
@@ -6,9 +6,11 @@ class Snake:
     #
     # self : the instance of the Snake itself
     #
-    def __init__(self):
+    def __init__(self, x, y):
         self.body = LinkedList()
         self.length = 1
+        self.x = x # starting x coordinate
+        self.y = y # starting y coordinate
     
     # function for growing the body of the snake object
     #
@@ -20,4 +22,31 @@ class Snake:
         self.body.ll_insert(None)
         self.body.ll_insert(None)
         self.length = self.ll_length()
-
+    
+    # moves the Snake instance to the left 1 block
+    # 
+    # self : the instance of the Snake itself
+    #
+    def move_left(self):
+        self.x -= 1
+    
+    # moves the Snake instance to the right 1 block
+    #
+    # self : the instance of the Snake itself
+    #
+    def move_right(self):
+        self.x += 1
+    
+    # moves the Snake instance upwards 1 block
+    #
+    # self : the instance of the Snake itself
+    #
+    def move_up(self):
+        self.y += 1
+    
+    # moves the Snake instance downward 1 block
+    #
+    # self : the instance of the Snake itself
+    #
+    def move_down(self):
+        self.y -= 1
