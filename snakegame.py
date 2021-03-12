b165463711a371147a99cc565a87f2c3e0132348
Eric Hernandez <eherna97@ucsc.edu>
2021-03-12 04:06:34 -0800

    added a small loop that will display the window and close on prompt

--- snakegame.py
+++ snakegame.py
@@ -3,9 +3,16 @@ import pygame
 
 # initialize pygame module
 pygame.init()
-# set w and h, make window resizable
+# in the block below, the following will happen:
+# set w and h, make window resizable, set title of screen, init the screen!
 screen_width = 700
 screen_height = 500
 flags = pygame.RESIZABLE | pygame.SCALED
-# start the actual screen
+pygame.display.set_caption("Classic Snake")
 screen = pygame.display.set_mode([screen_width, screen_height], flags)
+# simple loop for now that opens a window
+game_running = True  # simple window condition
+while game_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # in the case that the window is closed
+            game_running = False 
