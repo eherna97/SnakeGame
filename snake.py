78cc020d32959c269904346ee506a1b60b56c9c2
Eric Hernandez <eric.hdez2001@gmail.com>
2022-12-14 19:36:52 -0800

    changing the representation of snake ll -> list

--- snake.py
+++ snake.py
@@ -1,38 +1,32 @@
-from ll import LinkedList
+from ll import Node
+from typing import List, Tuple
 
 
-class Snake(LinkedList):
-    _color: tuple
+class Snake:
+    __body: List[Node]
+    __color: Tuple
 
-    # definition for the initialization of a Snake object
-    #
-    def __init__(self, color: tuple, x_val: int, y_val: int) -> None:
-        super().__init__(color, x_val, y_val)
-        self._color = color
-
-    # grows the body of a a Snake object by 4 Nodes
-    #
-    def grow(self, x_val: int, y_val: int) -> None:
-        self.ll_insert(self._color, x_val, y_val)
-        self.ll_insert(self._color, x_val, y_val)
-        self.ll_insert(self._color, x_val, y_val)
-        self.ll_insert(self._color, x_val, y_val)
-
-    # moves a Snake and its body along the grid
-    #
-    def move(self) -> None:
-        last_pos = [self.head.x, self.head.y]
-        curr = self.head.next
-        while curr != self.tail:
-            temp_last_pos = [curr.x, curr.y]
-            curr.x = last_pos[0]
-            curr.y = last_pos[1]
-            last_pos = temp_last_pos
-            curr = curr.next
-
-    # returns true if the Snake is out of bounds from the game grid
-    #
+    def __init__(self, color: Tuple, x_val: int, y_val: int):
+        self.__body.append(Node(color, x_val, y_val))
+        self.__color = color
+        
+    @property
+    def length(self) -> int:
+        return len(self.body)
+    
+    def grow(self, x: int, y: int) -> None:
+        self.__body.append(Node(self.__color, x, y))
+        self.__body.append(Node(self.__color, x, y))
+        self.__body.append(Node(self.__color, x, y))
+        self.__body.append(Node(self.__color, x, y))
+    
+    def move(self, position: Tuple[int, int]) -> None:
+        for node in self.__body:
+            node.x, node.y, position = position, (node.x, node.y)
+            # prev_position = (node.x, node.y)
+            # node.x, node.y = position
+            # position = prev_position
+    
     def out_of_bounds(self) -> bool:
-        if self.head.x in range(20, 820) and self.head.y in range(20, 620):
-            return False
-        return True
+        bounds = range(20, 820)
+        return not (self.__body[0].x in bounds and self.__body[0].y in bounds)
